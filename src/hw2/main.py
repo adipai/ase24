dfd65d2278c4290a7b2a49565cbf4e3cdee57ad0
Deepak Rajendran <deepak.rajendran123@gmail.com>
2024-01-15 17:04:13 -0500

    added cli shorthands

--- src/hw2/main.py
+++ src/hw2/main.py
@@ -26,22 +26,23 @@ run test-cases = args.run_tc
 """
 
 if __name__ == '__main__':
-    t = cli(settings(help_str))
+    t, opt_dir = settings(help_str)
+    t = cli(t, opt_dir)
     if(t['help']):
         print("You can use the following help: ")
         print(help_str)
     else:
-
         if(t['run_tc']=="all"):
             print("running all tests!")
             ts = TestSuite()
             ts.run_tests()
-        
+        elif(t['run_tc']==""):
+            pass
         elif(t['run_tc']!="None"):
             print("running test "+ t['run_tc'])
             ts = TestSuite()
             try:
-                egs[t['run_tc']]
+                egs[t['run_tc']]()
                 print(f"Test {t['run_tc']} passed.")
             except AssertionError as e:
                 print(f"Test {t['run_tc']} failed: {e}")
