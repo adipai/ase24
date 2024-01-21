f54b73dd5ec45df57ee75a65a0d9f5c28191544e
RISHI SINGHAL <rishi19194@iiitd.ac.in>
2024-01-21 00:36:10 -0500

    Added percentage for class samples for Task 1

--- src/hw3/task1.py
+++ src/hw3/task1.py
@@ -2,9 +2,11 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv("../../Data/soybean.csv")
+n = len(data)
 classes = np.unique(list(data['class!']), return_counts=True)[0]
 samples = np.unique(list(data['class!']), return_counts=True)[1]
 print("Number of classes: ", len(classes))
 for i in range(len(classes)):
     print("Class: ", classes[i])
-    print("Samples: ", samples[i])
+    print("Number of Class Samples: ", samples[i])
+    print("% Samples: ", (samples[i]/n)*100)
